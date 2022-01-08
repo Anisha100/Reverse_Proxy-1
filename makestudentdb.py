396a4612c4f0259e5fdeba63025ef3477e079b06
Aditya Mitra <adityaarghya0@gmail.com>
2022-01-08 07:23:43 +0530

    Delete makestudentdb.py

--- makestudentdb.py
+++
@@ -1,9 +0,0 @@
-from sqltasks import *
-file1 = open('studentlist.csv', 'r')
-resetStudentDb()
-Lines = file1.readlines()
-for line in Lines:
-	em=line.strip()
-	print('Adding '+em)
-	addStudent(em)
-	
